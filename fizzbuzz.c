2f31c9e08fbad26ac0988fc2da8bce49d19c11e2
Joona Suhonen <59951268+JoonaSuhonen@users.noreply.github.com>
2021-11-10 21:32:47 +0200

    Kirjootusvirheen korjaus

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,7 +2,7 @@
 
 void fizzbuzz() 
 {
-    for (int i = 0; i <=100; +i++) {
+    for (int i = 0; i <=100; i++) {
         if (i % 15 == 0){
              printf("%d FizzBuzz\n", i);
         } else if (i % 3 == 0) {
